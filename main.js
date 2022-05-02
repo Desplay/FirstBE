773eea33e8a7526cad5423de081b01ce4eba88bb
Desplay <desplayshido@gmail.com>
2022-05-02 18:12:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 // Code by Desplay !
-const Handing = require('./src/main-module.js');
+const Handing = require('./src-modules/main-module');
 const agrument = process.argv.splice(2);
-
-if (agrument[0].indexOf('.') === -1) {
-    console.log("Input ERROR !");
-    console.log("Please type right command !");
-    return;
-}
 Handing(agrument);
