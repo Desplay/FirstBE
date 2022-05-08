d1c49216af5ace60f869d1282027f4f000ef83ac
Desplay <desplayshido@gmail.com>
2022-05-08 16:21:59 +0700

    Update handing-module.js

--- src-modules/sub-modules/handing-module.js
+++ src-modules/sub-modules/handing-module.js
@@ -1,13 +1,18 @@
 const CopyFile = require('./copy-file-to-folder')
+const HandingCommand = require('./custom-command-type')
 
 const HandingFile = function(input) {
-    if(input[0].length === 1) {
+    if(input[0].length === 1) { // Is file then:
+        let TrueExtendCommand = HandingCommand(input[0][0], input[1]) && HandingCommand(input[0][0], input[2]) && HandingCommand(input[0][0], input[3])
+        if(TrueExtendCommand)
             CopyFile(input[0][0], input[1], input[2], input[3], input[4]);
-    } 
-    else {
-        var i = 0;
-        while(i < input[0].length){
-            CopyFile(input[0][i], input[1], input[2], input[3], input[4]);
+    }
+    else { // Is folder then:
+        let i = 0;
+        while(i < input[0].length) {
+            let TrueExtendCommand = HandingCommand(input[0][i], input[1]) && HandingCommand(input[0][i], input[2]) && HandingCommand(input[0][i], input[3])
+            if(TrueExtendCommand)
+                CopyFile(input[0][i], input[1], input[2], input[3], input[4]);
             i++;
         }
     }
