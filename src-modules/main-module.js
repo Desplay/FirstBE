4850f694b4489dfd46de0a48a38944ccaa77b85f
Desplay <desplayshido@gmail.com>
2022-05-08 16:21:45 +0700

    Update main-module.js

--- src-modules/main-module.js
+++ src-modules/main-module.js
@@ -1,12 +1,15 @@
-const CheckCommand = require('./sub-modules/check-command');
+const CheckCommand = require('./sub-modules/check-command')
 const FileDetails = require('./sub-modules/check-file-or-folder')
 const FileHanding = require('./sub-modules/handing-module')
 
 const MainModule = function (input) {
-  if(CheckCommand(input) === false)
+  if(!CheckCommand(input))
     return;
-  input[0] = FileDetails(input[0]);
-  FileHanding(input);
+  if (FileDetails(input[0]) != undefined) {
+    input[0] = FileDetails(input[0]);
+    FileHanding(input);
+  }
   return;
 }
+
 module.exports = MainModule;
