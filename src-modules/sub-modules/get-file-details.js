10f17a274c05c48719d822a3dc490441112280d0
Desplay <desplayshido@gmail.com>
2022-05-08 16:21:57 +0700

    Update get-file-details.js

--- src-modules/sub-modules/get-file-details.js
+++ src-modules/sub-modules/get-file-details.js
@@ -6,40 +6,38 @@ const GetFileDetailsInFolder = function(input) {
     var File = new Object();
     File.Path = input;
     var CheckFile = input.slice(input.lastIndexOf('/')+1, input.length)
-        if (input.indexOf('.') === -1) {
-            File = undefined;
+    if (input.indexOf('.') === -1) {
+        File = undefined;
+    }
+    else {
+        switch (CheckFile) {
+            case '.git' :
+                    File = 'source';
+                    break;
+            case 'main.js' :
+                    File = 'source';
+                    break;
+            case 'package.json' :
+                    File = 'source';
+                    break;
+            case 'README.md' :
+                    File = 'source';
+                    break;
+            default :
+                File.Name = input.slice(input.lastIndexOf('/')+1, input.lastIndexOf('.'))
+                File.FirstName = input.slice(input.lastIndexOf('/')+1, input.lastIndexOf('/')+2)
+                File.ExtName = path.extname(input)
+                File.Type = Type(File.ExtName)
+                File.Size = fs.statSync(input).size
+                var Date = fs.statSync(input).mtime
+                File.ModifiDate = new Object()
+                File.ModifiDate.Date = Date.getDate()
+                File.ModifiDate.Month = Date.getMonth()
+                File.ModifiDate.Year = Date.getUTCFullYear()
+                break;
         }
-        else {
-            switch (CheckFile) {
-                case '.git' :
-                    File = 'source';
-                    break;
-                case 'main.js' :
-                    File = 'source';
-                    break;
-                case 'package.json' :
-                    File = 'source';
-                    break;
-                case 'README.md' :
-                    File = 'source';
-                    break;
-                default :
-                    File.Name = input.slice(input.lastIndexOf('/')+1, input.lastIndexOf('.'))
-                    File.FirstName = input.slice(input.lastIndexOf('/')+1, input.lastIndexOf('/')+2)
-                    File.ExtName = path.extname(input)
-                    File.Type = Type(File.ExtName)
-                    File.Size = fs.statSync(input).size
-                    var Date = fs.statSync(input).ctime
-                    File.ModifiDate = new Object()
-                    File.ModifiDate.Date = Date.getDate()
-                    File.ModifiDate.Month = Date.getMonth()
-                    File.ModifiDate.Year = Date.getUTCFullYear()
-                    break;
-            }
-        }
+    }
     return File;
 }
-
-
 
 module.exports = GetFileDetailsInFolder;
